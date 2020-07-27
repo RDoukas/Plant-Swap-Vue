b3babaddb530cd49d3d304e21476f542957266be
Becca <rrdoukas@gmail.com>
2020-07-27 16:30:50 -0500

    updates minor things

--- src/views/ConversationsShow.vue
+++ src/views/ConversationsShow.vue
@@ -5,16 +5,16 @@
       <p>From: {{ message.username }}</p>
       <p>Sent: {{ relativeTime(message.created_at) }}</p>
     </div>
-    <!-- <form v-on:submit.prevent="createMessage()">
+    <form v-on:submit.prevent="createMessage()">
       <label>Send Message: </label>
       <input
         type="text"
         class="form-control"
         :placeholder="`Message ${partner.usernmae}`"
-        v-model="newMessage"
+        v-model="body"
       />
       <input type="submit" class="btn btn-primary" value="send" />
-    </form> -->
+    </form>
   </div>
 </template>
 
@@ -29,7 +29,7 @@ export default {
         messages: [],
       },
       partner: {},
-      // newMessage: "",
+      body: "",
     };
   },
   created: function() {
@@ -43,14 +43,19 @@ export default {
       });
   },
   methods: {
-    // createMessage: function() {
-    //   var params = {
-    //     body: this.newMessage,
-    //   };
-    //   axios.post("api/messages", params).then((response) => {
-    //     this.messages.push(response.data);
-    //   });
-    // },
+    createMessage: function() {
+      var params = {
+        body: this.body,
+      };
+      axios
+        .post("/api/messages", params)
+        .then((response) => {
+          this.messages.push(response.data);
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
     relativeTime: function(time) {
       return moment(time)
         .startOf("hour")
--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,61 +1,65 @@
 <template>
   <div class="users-show">
     <h2>First Name: {{ user.first_name }}</h2>
-    <h4>Last Name: {{ user.last_name}}</h4>
-    <h5>Username: {{user.username}}</h5>
-    <p>Email: {{user.email}}</p>
+    <h4>Last Name: {{ user.last_name }}</h4>
+    <h5>Username: {{ user.username }}</h5>
+    <p>Email: {{ user.email }}</p>
 
-  <br />
+    <br />
 
-   <form v-on:submit.prevent="editUser()">
-        <ul>
-          <li class="text-danger" v-for="error in errors">{{ error }}</li>
-        </ul>
-        <div class="form-group">
-          <label>First Name:</label>
-          <input type="text" class="form-control" v-model="user.first_name">
-        </div>
-        <div class="form-group">
-          <label>Last Name:</label>
-          <input type="text" class="form-control" v-model="user.last_name">
-        </div>
-        <div class="form-group">
-          <label>Username:</label>
-          <input type="text" class="form-control" v-model="user.username">
-        </div>
-        <div class="form-group">
-          <label>Email:</label>
-          <input type="text" class="form-control" v-model="user.email">
-        </div>
-        <div class="form-group">
-          <label>Password:</label>
-          <input type="password" class="form-control" v-model="password">
-        </div>
-           <div class="form-group">
+    <form v-on:submit.prevent="editUser()">
+      <ul>
+        <li class="text-danger" v-for="error in errors">{{ error }}</li>
+      </ul>
+      <div class="form-group">
+        <label>First Name:</label>
+        <input type="text" class="form-control" v-model="user.first_name" />
+      </div>
+      <div class="form-group">
+        <label>Last Name:</label>
+        <input type="text" class="form-control" v-model="user.last_name" />
+      </div>
+      <div class="form-group">
+        <label>Username:</label>
+        <input type="text" class="form-control" v-model="user.username" />
+      </div>
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="text" class="form-control" v-model="user.email" />
+      </div>
+      <div class="form-group">
+        <label>Password:</label>
+        <input type="password" class="form-control" v-model="password" />
+      </div>
+      <div class="form-group">
         <label>Password confirmation: </label>
-        <input type="password" class="form-control" v-model="passwordConfirmation">
+        <input
+          type="password"
+          class="form-control"
+          v-model="passwordConfirmation"
+        />
         <small class="text-danger" v-if="passwordConfirmation !== password"
           >Must match password</small
         >
       </div>
-        <input type="submit" class="btn btn-primary" value="Update">
-      </form>
-      <div id="destroyUser"> 
-        <button v-on:click="destroyUser()">Delete Your Profile</button>
-      </div>
+      <input type="submit" class="btn btn-primary" value="Update" />
+    </form>
+    <div id="destroyUser">
+      <button v-on:click="destroyUser()">Delete Your Profile</button>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  data: function () {
+  data: function() {
     return {
       errors: [],
       user: {},
     };
   },
-  created: function () {
+  created: function() {
     axios.get(`/api/users/${this.$route.params.id}`).then((response) => {
       this.user = response.data;
       console.log(this.user);
@@ -63,7 +67,7 @@ export default {
   },
 
   methods: {
-    destroyUser: function () {
+    destroyUser: function() {
       if (confirm("Are you sure you want to delete your profile?")) {
         axios.delete(`/api/users/${this.user.id}`).then((response) => {
           console.log("Your user has been successfully deleted", response.data);
@@ -71,7 +75,7 @@ export default {
         });
       }
     },
-    editUser: function () {
+    editUser: function() {
       var params = {
         first_name: this.firstName,
         last_name: this.lastName,
