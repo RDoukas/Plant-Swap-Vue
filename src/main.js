ce8c06d86d3b70099648c0e5415fbe90d0c315b2
Becca <rrdoukas@gmail.com>
2020-08-17 13:11:45 -0500

    works on front end

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "development"
     ? "https://chicago-plant-swap.herokuapp.com/"
     : "/";
 
