d498cc1d101f15c92ab6043c5fe8416d17894009
Becca <rrdoukas@gmail.com>
2020-08-17 13:21:24 -0500

    updates deployment

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "production"
     ? "https://chicago-plant-swap.herokuapp.com/"
     : "/";
 
