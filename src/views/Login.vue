3952d8558d40167c6f308964e396195d76c17863
Becca <rrdoukas@gmail.com>
2020-08-20 13:47:27 -0500

    updates login page bootstrap

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,55 @@
 <template>
   <div class="login">
-   
-    <form v-on:submit.prevent="submit()">
-      <h1>Login</h1>
-      <ul>
-        <li class="text-danger" v-for="error in errors">{{ error }}</li>
-      </ul>
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email">
-      </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" v-model="password">
-      </div>
-      <input type="submit" class="button" value="Submit">  
-    </form>
- 
-</div>
+    <div id="wrapper">
+      <div class="container-fluid">
+        <div class="user-form-container bg-white">
+          <h2>Login</h2>
+          <form v-on:submit.prevent="submit()">
+            <ul>
+              <li class="text-danger" v-for="error in errors">{{ error }}</li>
+            </ul>
+            <div class="form-group">
+              <label>Email:</label>
+              <input type="email" class="form-control" v-model="email">
+            </div>
+            <div class="form-group">
+              <label>Password:</label>
+              <input type="password" class="form-control" v-model="password">
+            </div>
+    
+            <div class="row">
+              <div class="col-xs-6 mt10-r">
+                <div class="checkbox mb25">
+                  <label class="custom-checkbox-wrapper">
+                    <span class="custom-checkbox-container">
+                        <input type="checkbox" value="true">
+                        <span class="custom-checkbox-icon"></span>
+                    </span>
+                    <small>Remember me</small>
+                  </label>
+                </div><!-- End .checkbox -->
+              </div><!-- End .col-xs-6 -->
+
+              <div class="col-xs-6 mt10-r text-right">
+                <a href="#"><small>Recover Password?</small></a>
+              </div><!-- End .col-xs-6 -->
+            </div><!-- End .row -->
+
+            <button class="btn btn-custom btn-block min-width">Login</button>
+            <div class="mb20"></div><!-- margin -->
+            <div class="text-center">
+              <small>Not a member yet?</small><br>
+              <a href="/signup">Signup Now</a>
+            </div>
+          </form>
+        </div><!-- End .user-form-container -->
+      </div><!-- End .container-fluid -->
+
+    </div><!-- End #wrapper -->
+    
+      
+  
+    </div>
   </div>
 </template>
 
@@ -26,33 +58,33 @@ import axios from "axios";
 
 export default {
   data: function() {
-    return {
-      email: "",
-      password: "",
-      errors: [],
-    };
+  return {
+    email: "",
+    password: "",
+    errors: [],
+  };
   },
   methods: {
-    submit: function() {
-      var params = {
-        email: this.email,
-        password: this.password,
-      };
-      axios
-        .post("/api/sessions", params)
-        .then((response) => {
-          axios.defaults.headers.common["Authorization"] =
-            "Bearer " + response.data.jwt;
-          localStorage.setItem("jwt", response.data.jwt);
-          localStorage.setItem("current_user", response.data.user_id);
-          this.$router.push("/");
-        })
-        .catch((error) => {
-          this.errors = ["Invalid email or password."];
-          this.email = "";
-          this.password = "";
-        });
-    },
+  submit: function() {
+    var params = {
+    email: this.email,
+    password: this.password,
+    };
+    axios
+    .post("/api/sessions", params)
+    .then((response) => {
+      axios.defaults.headers.common["Authorization"] =
+      "Bearer " + response.data.jwt;
+      localStorage.setItem("jwt", response.data.jwt);
+      localStorage.setItem("current_user", response.data.user_id);
+      this.$router.push("/");
+    })
+    .catch((error) => {
+      this.errors = ["Invalid email or password."];
+      this.email = "";
+      this.password = "";
+    });
+  },
   },
 };
 </script>
