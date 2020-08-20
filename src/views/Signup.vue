93767cb5faef6eef3ce6d5f1514bb8e08cf42dd6
Becca <rrdoukas@gmail.com>
2020-08-20 14:06:06 -0500

    installs bootstrap on sign up page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,6 +1,136 @@
 <template>
   <div class="signup">
-    <form v-on:submit.prevent="submit()">
+    <div id="wrapper">
+      <div class="container-fluid">
+        <div class="user-form-container register-form bg-white">
+          <h2 class="mb30">Signup</h2>
+          <form v-on:submit.prevent="submit()">
+            <ul>
+              <li class="text-danger" v-for="error in errors">{{ error }}</li>
+            </ul>
+            <div class="row">
+              <div class="col-sm-6 ">
+                <div class="form-group">
+                  <label>First Name:</label>
+                  <input type="text" class="form-control" v-model="firstName" />
+                </div>
+                <!-- End .from-group -->
+              </div>
+              <!-- End .col-sm-6 -->
+              <div class="col-sm-6 ">
+                <div class="form-group">
+                  <label>Last Name:</label>
+                  <input type="text" class="form-control" v-model="lastName" />
+                </div>
+                <!-- End .from-group -->
+              </div>
+              <!-- End .col-sm-6 -->
+            </div>
+            <!-- End .row -->
+
+            <div class="row">
+              <div class="col-sm-6 ">
+                <div class="form-group">
+                  <label>Email:</label>
+                  <input type="email" class="form-control" v-model="email" />
+                </div>
+                <!-- End .from-group -->
+              </div>
+              <!-- End .col-sm-6 -->
+              <div class="col-sm-6 ">
+                <div class="form-group">
+                  <label>Username:</label>
+                  <input type="text" class="form-control" v-model="username" />
+                  <div>
+                    <small>
+                      {{ 20 - username.length }} characters remaining</small
+                    >
+                  </div>
+                </div>
+                <!-- End .from-group -->
+              </div>
+              <!-- End .col-sm-6 -->
+            </div>
+            <!-- End .row -->
+
+            <div class="row">
+              <div class="col-sm-6 ">
+                <div class="form-group">
+                  <label>Password:</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    v-model="password"
+                  />
+                  <div>
+                    <small
+                      class="text-danger"
+                      v-if="password.length > 0 && password.length < 6"
+                    >
+                      Must be at least 6 characters</small
+                    >
+                  </div>
+                  <!-- End .from-group -->
+                </div>
+              </div>
+              <!-- End .col-sm-6 -->
+              <div class="col-sm-6 ">
+                <div class="form-group">
+                  <label>Password confirmation: </label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    v-model="passwordConfirmation"
+                  />
+                  <small
+                    class="text-danger"
+                    v-if="passwordConfirmation !== password"
+                    >Must match password</small
+                  >
+                </div>
+                <!-- End .from-group -->
+              </div>
+              <!-- End .col-sm-6 -->
+            </div>
+            <!-- End .row -->
+
+            <div class="mb10"></div>
+            <!-- space -->
+
+            <div class="form-group mb0">
+              <div class="checkbox">
+                <label class="custom-checkbox-wrapper">
+                  <span class="custom-checkbox-container">
+                    <input type="checkbox" value="true" />
+                    <span class="custom-checkbox-icon"></span>
+                  </span>
+                  <span
+                    >I have read and agree on the
+                    <a href="#">Privacy Policy</a>.</span
+                  >
+                </label>
+              </div>
+              <!-- End .checkbox -->
+            </div>
+            <!-- End .form-group -->
+
+            <div class="form-group mb5">
+              <input
+                type="submit"
+                class="btn btn-custom min-width"
+                value="Signup"
+              />
+            </div>
+            <!-- End .from-group -->
+          </form>
+        </div>
+        <!-- End .register-form-container -->
+      </div>
+      <!-- End .container-fluid -->
+    </div>
+    <!-- End #wrapper -->
+
+    <!-- <form v-on:submit.prevent="submit()">
       <div class="text-center">
         <h1>Signup</h1>
         <img v-if="status" :src="`https://http.cat/${status}`" alt="" />
@@ -56,7 +186,7 @@
         >
       </div>
       <input type="submit" class="button" value="Signup" />
-    </form>
+    </form> -->
   </div>
 </template>
 
