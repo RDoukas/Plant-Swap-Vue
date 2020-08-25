354840c07cdbfc8b7812b69abc16b39c41e51e51
Becca <rrdoukas@gmail.com>
2020-08-24 21:05:40 -0500

    updates profile page

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,74 +1,77 @@
 <template>
   <div class="users-show">
-    <div id="wrapper">
-      <div class="container-fluid">
-        <div class="user-form-container register-form bg-white">
-          <h2 class="mb30">Profile Information</h2>
-          <form v-on:submit.prevent="editUser()">
-            <!-- <ul>
-              <li class="text-danger" v-for="error in errors">{{ error }}</li>
-            </ul> -->
-            <div class="row">
-              <div class="col-sm-6 ">
-                <div class="form-group">
-                  <label>First Name:</label>
-                  <input type="text" class="form-control" v-model="user.first_name" />
-                </div><!-- End .from-group -->
-              </div><!-- End .col-sm-6 -->
-              <div class="col-sm-6 ">
-                <div class="form-group">
-                  <label>Last Name:</label>
-                  <input type="text" class="form-control" v-model="user.last_name" />   
-                </div><!-- End .from-group -->
-              </div><!-- End .col-sm-6 -->
-            </div><!-- End .row -->
+    <div class="container-fluid">
+      <form v-on:submit.prevent="editUser()">
+        <ul>
+          <li class="text-danger" v-for="error in errors">{{ error }}</li>
+        </ul>
 
-            <div class="row">
-              <div class="col-sm-6 ">
-                <div class="form-group">
-                  <label>Username:</label>
-                  <input type="text" class="form-control" v-model="user.username" />
-                </div><!-- End .from-group -->
-              </div><!-- End .col-sm-6 -->
-              <div class="col-sm-6 ">
-                <div class="form-group">
-                  <label>Email:</label>
-                  <input type="text" class="form-control" v-model="user.email" />   
-                </div><!-- End .from-group -->
-              </div><!-- End .col-sm-6 -->
-            </div><!-- End .row -->
-            
-            <div class="row">
-              <div class="col-sm-6 ">
-                <div class="form-group">
-                  <label>Password:</label>
-                  <input type="password" class="form-control" v-model="password" />
-                </div><!-- End .from-group -->
-              </div><!-- End .col-sm-6 -->
-              <div class="col-sm-6 ">
-                <div class="form-group">
-                  <label>Password confirmation: </label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    v-model="passwordConfirmation"
-                  />
-                  <small class="text-danger" v-if="passwordConfirmation !== password">Must match password</small>
-                </div><!-- End .from-group -->
-              </div><!-- End .col-sm-6 -->
-            </div><!-- End .row -->
-            <div class="mb10"></div><!-- space -->
-              <input type="submit" class="btn btn-custom min-width" value="Update Profile">
-            </div><!-- End .from-group -->
-          </form>
-        </div><!-- End .register-form-container -->
-      </div><!-- End .container-fluid -->
-    </div><!-- End #wrapper -->
-    
-      
-    <!-- <div id="destroyUser">
-      <button class="btn btn-custom min-width" v-on:click="destroyUser()">Delete Your Profile</button>
-    </div> -->
+        <div class="row">
+          <h2 class="title custom mb30">Profile Information</h2>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>First Name:</label>
+              <input
+                type="text"
+                class="form-control"
+                v-model="user.first_name"
+              />
+            </div>
+            <div class="form-group">
+              <label>Last Name:</label>
+              <input
+                type="text"
+                class="form-control"
+                v-model="user.last_name"
+              />
+            </div>
+          </div>
+          <!-- End .col-md-6 -->
+
+          <div class="mb30 visible-sm visible-xs"></div>
+          <!-- margin -->
+        </div>
+        <!-- end .row -->
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Username:</label>
+              <input type="text" class="form-control" v-model="user.username" />
+            </div>
+            <div class="form-group">
+              <label>Email:</label>
+              <input type="text" class="form-control" v-model="user.email" />
+            </div>
+            <div class="form-group">
+              <label>Password:</label>
+              <input type="password" class="form-control" v-model="password" />
+            </div>
+            <div class="form-group">
+              <label>Password confirmation: </label>
+              <input
+                type="password"
+                class="form-control"
+                v-model="passwordConfirmation"
+              />
+              <small
+                class="text-danger"
+                v-if="passwordConfirmation !== password"
+                >Must match password</small
+              >
+            </div>
+          </div>
+        </div>
+        <input type="submit" class="btn btn-custom" value="Update Profile" />
+      </form>
+
+      <div class="mb25 mb15-sm"></div>
+      <div id="destroyUser">
+        <button type="submit" class="btn btn-custom" v-on:click="destroyUser()">
+          Delete Profile
+        </button>
+      </div>
+      <div class="mb25 mb15-sm"></div>
+    </div>
   </div>
 </template>
 
