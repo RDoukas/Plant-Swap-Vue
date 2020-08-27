a07efff89307a16536580ba9e97d15186e156deb
Becca <rrdoukas@gmail.com>
2020-08-27 13:48:33 -0500

    updates heroku url on main js page

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://chi-plant-swap.herokuapp.com/"
+    ? "https://git.heroku.com/chi-plant-swap.git"
     : "/";
 // process.env.NODE_ENV === "development" ? "https://localhost:3000/" : "/";
 
