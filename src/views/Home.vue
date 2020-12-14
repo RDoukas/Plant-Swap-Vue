b94efa7d640e1218f1c2bcf10c4b95b9c96d09c1
Becca <rrdoukas@gmail.com>
2020-12-14 14:21:03 -0600

    updates about us section and format on app.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="home">
-    <div id="wrapper">
-      <h1>Welcome to Chicago Plant Swap</h1>
-      <div class="row">
-        <h3>About <span class="light text-custom">Us</span></h3>
-        <p>
-          Chicago Plant Swapp is an app that connects plant lovers across the
-          Chicagoland area. All plants are by donation and there is no charge
-          for plants.
-        </p>
-        <p>
-          After having an overflowing collection of plants we decided what
-          better than to share them with other plant lovers who we know will
-          love our babies! You can swap anything from fully potted plants to
-          propogations to even seeds!
-        </p>
+    <div class="container-fluid"></div>
+    <!-- End .container-fluid -->
+
+    <!-- <div class="mb60 mb50-sm mb45-xs"></div> -->
+    <!-- margin -->
+
+    <div class="bg-white pt60 pb60 pt50-sm pb50-sm">
+      <div class="container-fluid">
+        <header class="title-block text-center">
+          <h2 class="title-border custom text-center">
+            <span>About Us</span>
+          </h2>
+          <p>
+            Chicago Plant Swapp is an app that connects plant lovers across the
+            Chicagoland area. All plants are by donation and there is no charge
+            for plants.
+          </p>
+          <p>
+            After having an overflowing collection of plants we decided what
+            better than to share them with other plant lovers who we know will
+            love our babies! You can swap anything from fully potted plants to
+            propogations to even seeds!
+          </p>
+        </header>
+        <!-- End .clients-carousel -->
       </div>
-      <!-- End .col-md-6 -->
+      <!-- End .container-fluid -->
     </div>
-    <!-- End .row -->
+    <!-- End .bg-white -->
+
+    <div class="mb60 mb50-sm"></div>
+    <!-- margin -->
+
+    <div class="mb65 mb60-sm"></div>
   </div>
 </template>
 
