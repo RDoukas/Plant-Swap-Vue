c68a84383e32300bf50f688bbd21e308c289715a
Becca <rrdoukas@gmail.com>
2020-12-15 16:26:24 -0600

    updates ads show page

--- src/views/AdsShow.vue
+++ src/views/AdsShow.vue
@@ -1,123 +1,168 @@
 <template>
   <div class="ads-show">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-lg-6 col-md-6">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="product-gallery-container">
+          <div class="product-zoom-wrapper">
+            <div class="product-zoom-container">
+              <img :src="ad.image_url" alt="" width="300" />
+            </div><!-- End .product-zoom-container -->
+          </div><!-- End .product-zoom-wrapper -->
+        </div><!-- End .product-gallery-container -->
+      </div><!-- End .col-md-6 -->
+
+      <div class="clearfix visible-sm"></div><!-- end .clearfix -->
+
+      <div class="col-md-6">
         <div class="product-details">
-          <div class="product-details-img">
-          <div class="tab-content jump">
-            <div id="shop-details-2"
-            class="tab-pane active large-img-style">
-            <img :src="ad.image_url" alt="" width="300" />
-            </div>
-            <!-- End .product-zoom-wrapper -->
-          </div>
-          </div>
-        </div>
-        </div>
-        <div class="col-lg-6 col-md-6">
-        <div class="product-details-content ml-70" >
-          <h2>{{ ad.title }}</h2>
-          <h5>Plant Parent: {{ ad.username }}</h5>
-          <h6>Posted on: {{ ad.created_at }}</h6>
-          </br>
-          <p>{{ ad.description }}</p>
-          </br >
-          <h5>Categories:</h5>
-          <ul>
-            <div v-for="category in ad.categories">{{ category.name }}
-            </div>
-          </ul>
-        </div>
-        </br>
-        <div class="blog-reply-wrapper mt-50">
-          <div v-if="!ad.owner">
-          <form class="blog-form" action="#" v-on:submit.prevent="createConversation()">
-            <label>Send Message: </label>
-            <div class="row">
-              <div class="col-md-12">
-              <div class="text-leave">
-                <textarea
-                type="text"
-                class="form-control"
-                :placeholder="`Message...`"
-                v-model="newMessage"
-                cols="30"
-                rows="3"
-                ></textarea>
-                <input type="submit" value="SEND MESSAGE"/>
-              </div>
-              </div>
-              <!-- End .panel -->
-            </div>
-          </form>
-          </div>
-        </div>
+          <h2 class="product-title">{{ad.title}}t</h2>
 
+          <div class="product-desc-box">
+            <ul>
+              <li><strong>Plant Parent: </strong> {{ad.username}}</li>
+              <li><strong>Date Posted: </strong>{{ad.created_at}}</li>
+              <li><strong>Description: </strong> {{ad.description}}</li>
+            </ul>
+          </div><!-- End .product-desc-box -->
+
+          <div class="row">
+            <div class="col-xs-6">
       
-        </div>
-        
-        <div v-if="ad.owner">
-      <!-- Button trigger modal -->
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editAdModal">
-          Edit
-          </button>
-          <button class="btn btn-primary" v-on:click="destroyAd()">Delete</button>
-        </div>
+            </div><!-- End .col-xs-6 -->
+            <div class="col-xs-6">
+            </div><!-- End .col-xs-6 -->
+          </div><!-- End .row -->
+          <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel">
+              <div class="panel-heading" role="tab" id="headingOne2">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne2" aria-expanded="true" aria-controls="collapseOne2">
+                    Categories
+                  </a>
+                </h4>
+              </div><!-- End .panel-heading -->
+              <div id="collapseOne2" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne2">
+                <div class="panel-body">
+                  <div v-for="category in ad.categories">{{ category.name }}</div>
+                </div><!-- End .panel-body -->
+              </div><!-- End .panel-collapse -->
+            </div><!-- End .panel -->
+          </div><!-- End .panel-group -->
+        </div><!-- End .product-details -->
+      </div><!-- End .col-md-6 -->
+    </div><!-- End .row -->
+    
+  </div><!-- End .container-fluid -->
 
-    <!-- Edit Ad Modal -->
-        <div class="modal fade" id="editAdModal" tabindex="-1" role="dialog" aria-labelledby="editAdModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="editAdModalLabel">Edit Ad</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-            </div>
-            <div class="modal-body">
-            <form v-on:submit.prevent="editAd()">
-              <ul>
-              <li class="text-danger" v-for="error in errors">{{ error }}</li>
-              </ul>
-              <div class="form-group">
-              <label>Title:</label>
-              <input type="text" class="form-control" v-model="ad.title" />
-              </div>
-              <div class="form-group">
-              <label>Description: </label>
-              <input type="text" class="form-control" v-model="ad.description" />
-              </div>
-              <div class="form-group">
-              <label>Image:</label>
-              <input
-                class="form-control"
-                type="file"
-                v-on:change="setFile($event)"
-                ref="fileInput"
-              />
-              </div>
-              <div class="form-group">
-              <div v-for="category in categories">
-                <input
-                type="checkbox"
-                :id="category.id"
-                :value="category.id"
-                v-model="categoryIds"
-                />
-                <label :for="category.id">{{ category.name }}</label>
-              </div>
-              {{ categoryIds }}
-              </div>
-            <input type="submit" class="btn btn-primary" value="Update" />
-          </form> 
-          </div> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- old code -->
+  <div class="container-fluid">
+
+    <div class="blog-reply-wrapper mt-50">
+      <div v-if="!ad.owner">
+      <form class="blog-form" action="#" v-on:submit.prevent="createConversation()">
+      <label>Send Message: </label>
+      <div class="row">
+        <div class="col-md-12">
+        <div class="text-leave">
+        <textarea
+        type="text"
+        class="form-control"
+        :placeholder="`Message...`"
+        v-model="newMessage"
+        cols="30"
+        rows="3"
+        ></textarea>
+        <input type="submit" value="SEND MESSAGE"/>
         </div>
         </div>
+        <!-- End .panel -->
       </div>
+      </form>
       </div>
-      <!-- End .row -->
     </div>
+
+    
+    </div>
+    
+    <div v-if="ad.owner">
+    <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editAdModal">
+      Edit
+      </button>
+      <button class="btn btn-primary" v-on:click="destroyAd()">Delete</button>
+    </div>
+
+  <!-- Edit Ad Modal -->
+    <div class="modal fade" id="editAdModal" tabindex="-1" role="dialog" aria-labelledby="editAdModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+      <div class="modal-content">
+      <div class="modal-header">
+      <h5 class="modal-title" id="editAdModalLabel">Edit Ad</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>
+      <div class="modal-body">
+      <form v-on:submit.prevent="editAd()">
+        <ul>
+        <li class="text-danger" v-for="error in errors">{{ error }}</li>
+        </ul>
+        <div class="form-group">
+        <label>Title:</label>
+        <input type="text" class="form-control" v-model="ad.title" />
+        </div>
+        <div class="form-group">
+        <label>Description: </label>
+        <input type="text" class="form-control" v-model="ad.description" />
+        </div>
+        <div class="form-group">
+        <label>Image:</label>
+        <input
+        class="form-control"
+        type="file"
+        v-on:change="setFile($event)"
+        ref="fileInput"
+        />
+        </div>
+        <div class="form-group">
+        <div v-for="category in categories">
+        <input
+        type="checkbox"
+        :id="category.id"
+        :value="category.id"
+        v-model="categoryIds"
+        />
+        <label :for="category.id">{{ category.name }}</label>
+        </div>
+        {{ categoryIds }}
+        </div>
+      <input type="submit" class="btn btn-primary" value="Update" />
+      </form> 
+      </div> 
+    </div>
+    </div>
+    </div>
+    </div>
+    <!-- End .row -->
+  </div>
   </div>
 </template>
 
@@ -129,76 +174,76 @@ import axios from "axios";
 export default {
   data: function() {
   return {
-    errors: [],
-    ad: {},
-    categories: [],
-    categoryIds: [],
-    conversation: {},
-    messages: [],
-    newMessage:"",
+  errors: [],
+  ad: {},
+  categories: [],
+  categoryIds: [],
+  conversation: {},
+  messages: [],
+  newMessage:"",
   };
   },
   created: function() {
   axios.get(`/api/ads/${this.$route.params.id}`).then((response) => {
-    this.ad = response.data;
-    console.log(this.ad);
-    this.categoryIds = this.ad.categories.map((category) => category.id);
+  this.ad = response.data;
+  console.log(this.ad);
+  this.categoryIds = this.ad.categories.map((category) => category.id);
   });
   axios.get(`/api/categories/`).then((response) => {
-    this.categories = response.data;
-    console.log(response.data);
+  this.categories = response.data;
+  console.log(response.data);
   });
   },
 
   methods: {
   setFile: function(event) {
-    if (event.target.files.length > 0) {
-    this.ad.imageUrl = event.target.files[0];
-    }
+  if (event.target.files.length > 0) {
+  this.ad.imageUrl = event.target.files[0];
+  }
   },
   editAd: function() {
-    var formData = new FormData();
-    formData.append("title", this.ad.title);
-    formData.append("description", this.ad.description);
-    formData.append("image_url", this.ad.imageUrl);
-    formData.append("category_ids", JSON.stringify(this.categoryIds));
+  var formData = new FormData();
+  formData.append("title", this.ad.title);
+  formData.append("description", this.ad.description);
+  formData.append("image_url", this.ad.imageUrl);
+  formData.append("category_ids", JSON.stringify(this.categoryIds));
 
-    console.log(this.categoryIds);
+  console.log(this.categoryIds);
 
-    if (confirm("Are you sure you'd like to update your ad?")) {
-    axios
-      .patch(`/api/ads/${this.ad.id}`, formData)
-      .then((response) => {
-      console.log("Your ad has been updated!", response.data);
-      })
-      .catch((error) => {
-      this.errors = error.response.data.errors;
-      });
-    }
-  },
-  destroyAd: function() {
-    if (confirm("Are you sure you want to delete this ad?")) {
-    axios.delete(`/api/ads/${this.ad.id}`).then((response) => {
-      console.log(response.data);
-      this.$router.push("/ads");
-    });
-    }
-  },
-  createConversation: function() {
-    var params = {
-    recipient_id: this.ad.user_id,
-    ad_id: this.ad.id,
-    message: this.newMessage
-    };
-    axios
-    .post(`/api/conversations`, params)
+  if (confirm("Are you sure you'd like to update your ad?")) {
+  axios
+    .patch(`/api/ads/${this.ad.id}`, formData)
     .then((response) => {
-      console.log(response.data);
-      this.$router.push(`/conversations/${response.data.id}`);
+    console.log("Your ad has been updated!", response.data);
     })
     .catch((error) => {
-      this.errors = error.response.data.errors;
+    this.errors = error.response.data.errors;
     });
+  }
+  },
+  destroyAd: function() {
+  if (confirm("Are you sure you want to delete this ad?")) {
+  axios.delete(`/api/ads/${this.ad.id}`).then((response) => {
+    console.log(response.data);
+    this.$router.push("/ads");
+  });
+  }
+  },
+  createConversation: function() {
+  var params = {
+  recipient_id: this.ad.user_id,
+  ad_id: this.ad.id,
+  message: this.newMessage
+  };
+  axios
+  .post(`/api/conversations`, params)
+  .then((response) => {
+    console.log(response.data);
+    this.$router.push(`/conversations/${response.data.id}`);
+  })
+  .catch((error) => {
+    this.errors = error.response.data.errors;
+  });
   },
   },
 };
