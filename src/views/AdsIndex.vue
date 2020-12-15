98498ae414a6fe23bf0edd721ae5eec8387ed1ee
Becca <rrdoukas@gmail.com>
2020-12-15 15:45:26 -0600

    sets up ads  index page

--- src/views/AdsIndex.vue
+++ src/views/AdsIndex.vue
@@ -1,5 +1,149 @@
 <template>
   <div class="ads-index">
+    <div class="container-fluid">
+      <div class="row flex-shop">
+        <div class="col-md-9 col-md-push-3">
+          <div class="category-filter-row">
+            <div class="right"></div>
+            <!-- end .right -->
+            <div class="left">
+              <div class="filter-container filter-sort">
+                <label>Sort by:</label>
+                <select class="form-control input-sm">
+                  <option value="Date">Date Posted</option>
+                  <option value="Username">Plant Parent</option>
+                  <option value="Title">Title</option>
+                </select>
+              </div>
+              <!-- End .filter-sort -->
+              <div class="filter-container filter-show"></div>
+              <!-- End .filter-show -->
+            </div>
+            <!-- End .left -->
+          </div>
+          <!-- End .category-filter-row -->
+
+          <div class="product product-list" v-for="ad in ads">
+            <div class="row">
+              <div class="col-md-4 col-sm-5">
+                <div class="product-top">
+                  <figure>
+                    <a href="`/ads/${ad.id}`" title="Product Name">
+                      <img
+                        src="`ad.image_url`"
+                        alt="Product image"
+                        class="product-image"
+                      />
+                    </a>
+                  </figure>
+                </div>
+                <!-- End .product -->
+              </div>
+              <!-- End .col-md-4 -->
+              <div class="col-md-8 col-sm-7">
+                <div class="product-content">
+                  <h3 class="product-title">
+                    <a href="`/ads/${ad.id}`">{{ ad.title }}</a>
+                  </h3>
+                  <p>Plant Parent: {{ ad.username }}</p>
+                  <p>Date Posted: {{ ad.created_at }}</p>
+                  <p>
+                    {{ ad.description }}
+                  </p>
+                </div>
+                <!-- End .product-content -->
+              </div>
+              <!-- End .col-md-8 -->
+            </div>
+            <!-- end .Row -->
+          </div>
+          <!-- End .product -->
+
+          <div class="mb20 mb15-sm"></div>
+          <!-- End margin -->
+        </div>
+        <!-- End .col-md-9 -->
+
+        <aside class="col-md-3 col-md-pull-9 sidebar shop-sidebar">
+          <div class="widget">
+            <div class="filter-group-widget">
+              <div
+                class="panel-group"
+                role="tablist"
+                aria-multiselectable="true"
+              >
+                <div class="panel">
+                  <div class="panel-heading" role="tab" id="brandFilter-header">
+                    <h4 class="panel-title">
+                      <a
+                        data-toggle="collapse"
+                        href="#brandFilter"
+                        aria-expanded="true"
+                        aria-controls="brandFilter"
+                      >
+                        Brand Filter
+                        <span class="panel-icon"></span>
+                      </a>
+                    </h4>
+                  </div>
+                  <!-- End .panel-heading -->
+                  <div
+                    id="brandFilter"
+                    class="panel-collapse collapse in"
+                    role="tabpanel"
+                    aria-labelledby="brandFilter-header"
+                  >
+                    <div class="panel-body">
+                      <ul class="filter-brand-list">
+                        <li>
+                          <a href="#"
+                            ><i class="fa fa-angle-right"></i>Yikes &amp; Sports
+                            <span>(11)</span></a
+                          >
+                        </li>
+                        <li>
+                          <a href="#"
+                            ><i class="fa fa-angle-right"></i>Smittzy &amp; Jane
+                            <span>(7)</span></a
+                          >
+                        </li>
+                        <li>
+                          <a href="#"
+                            ><i class="fa fa-angle-right"></i>Susan's Secrets
+                            <span>(10)</span></a
+                          >
+                        </li>
+                        <li>
+                          <a href="#"
+                            ><i class="fa fa-angle-right"></i>Puma &amp; Cougar
+                            <span>(14)</span></a
+                          >
+                        </li>
+                        <li>
+                          <a href="#"
+                            ><i class="fa fa-angle-right"></i>Jane Shirts
+                            <span>(9)</span></a
+                          >
+                        </li>
+                      </ul>
+                    </div>
+                    <!-- End .panel-body -->
+                  </div>
+                  <!-- End .panel-collapse -->
+                </div>
+                <!-- End .panel -->
+              </div>
+              <!-- End .panel-group -->
+            </div>
+            <!-- End .filter-widget -->
+          </div>
+          <!-- End .widget -->
+        </aside>
+      </div>
+      <!-- End .row -->
+    </div>
+    <!-- End .container-fluid -->
+
     <!-- old code  -->
     <div class="shop-area pt-95 pb-100">
       <div class="container">
