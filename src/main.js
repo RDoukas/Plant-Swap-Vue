9a4e94a9f428747c2dab23745d20b5bfe2865d14
Becca <rrdoukas@gmail.com>
2021-02-24 16:57:12 -0600

    production

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "production"
     ? "https://polar-badlands-74087.herokuapp.com/"
     : "";
 
